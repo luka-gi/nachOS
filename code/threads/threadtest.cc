d57de7674c10cb2b1c1ef9e69c0af3ee5e0f0e2c
luka-gi <lupblanch@hotmail.com>
2021-09-04 20:24:00 -0500

    added scripts for ease of use

--- code/threads/threadtest.cc
+++ code/threads/threadtest.cc
@@ -18,51 +18,13 @@ void SimpleThread(int param)
 
     for (num = 0; num < 5; num++)
     {
-        printf("*** thread %d looped %d times\n", param, num);
+        printf("*** thread %d looped %d times\n", param, partNum);
         currentThread->Yield();
     }
 }
 
 //Begin code changes by Lucas Blanchard
-void PingPong(int loops)
-{
-    int num;
 
-    for (num = 1; num <= loops; num++)
-    {
-        if (num % 2 != 0)
-        {
-            printf("%s: \"Ping\" looped %d times\n", currentThread->getName(), num);
-        }
-        else
-        {
-            printf("%s: \"Pong\" looped %d times\n", currentThread->getName(), num);
-        }
-        currentThread->Yield();
-    }
-}
-
-void HelloWorld(int loops)
-{
-    int num;
-
-    for (num = 1; num <= loops; num++)
-    {
-        printf("%s: \"Hello World!\" looped %d times\n", currentThread->getName(), num);
-        currentThread->Yield();
-    }
-}
-
-void CountUp(int loops)
-{
-    int num;
-
-    for (num = 1; num <= loops; num++)
-    {
-        printf("%s: \"Count to %d: %d\"\n", currentThread->getName(), loops, num);
-        currentThread->Yield();
-    }
-}
 //End code changes by Lucas Blanchard
 
 //----------------------------------------------------------------------
@@ -73,14 +35,8 @@ void CountUp(int loops)
 //Begin code changes by Lucas Blanchard
 void ThreadTest()
 {
-    Thread *t1, *t2, *t3;
-
-    t1 = new Thread("T_1");
-    t2 = new Thread("T_2");
-    t3 = new Thread("T_3");
-
-    t1->Fork(HelloWorld, 3);
-    t2->Fork(PingPong, 4);
-    t3->Fork(CountUp, 5);
+    Thread *t1 = new Thread("T_1");
+    t1->Fork(SimpleThread, 5);
+    currentThread->Finish();
 }
 //End code changes by Lucas Blanchard
